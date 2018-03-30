dea3d6ce286766bd3db3003cecd47c083443e41c
ZiyadKhalil <ziyadhkhalil@gmail.com>
2018-03-30 13:30:55 +0200

    #0 units package created to include units board package created to include the circuit added packages assembler and memory

---
+++ src/assembler/FormatChecker.java
@@ -0,0 +1,14 @@
+package assembler;
+
+public class FormatChecker {
+    String instruction;
+    String format;
+    public FormatChecker(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public String getFormat() {
+        //TODO: DO IT YA HAGER
+        return format;
+    }
+}
--- src/units/Mux.java
+++ src/units/Mux.java
@@ -1,7 +1,17 @@
 package units;
 
 public class Mux {
-    boolean firstOperand;
-    boolean secondOpereand;
+    int firstInput;
+    int secondInput;
+    boolean selection;
 
+    public Mux(int firstInput, int secondInput, boolean selection) {
+        this.firstInput = firstInput;
+        this.secondInput = secondInput;
+        this.selection = selection;
+    }
+    public int select(){
+        if(selection) return secondInput;
+        return firstInput;
+    }
 }
