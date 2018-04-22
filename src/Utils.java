75c3aaf76c26063e8f64f656df3ac76e63f903c3
ZiyadKhalil <ziyadhkhalil@gmail.com>
2018-04-22 09:47:44 +0200

    Merge branch 'notMaster' of https://github.com/ZiyadKhalil/MipsSimulator
    # Conflicts:
    #	.idea/workspace.xml
    #	src/memory/MemElement.java

--- src/Utils.java
+++ src/Utils.java
@@ -4,12 +4,12 @@ import units.Controller;
 
 
 public class Utils {
-    public static boolean[] Haha (Instruction instruction){
+    public static boolean[] Haha(Instruction instruction) {
         String p = instruction.getOpCode();
         boolean[] b;
-        if (!(instruction.getFormat()== InstructionFormat.R_FORMAT)) {
+        if (!(instruction.getFormat() == InstructionFormat.R_FORMAT)) {
             b = new boolean[p.length()];
-            for(int k=0;k<p.length();k++){
+            for (int k = 0; k < p.length(); k++) {
                 if (p.charAt(k) == '1') {
                     b[p.length() - k - 1] = true;
                 } else {
@@ -17,26 +17,51 @@ public class Utils {
                 }
             }
         } else {
-            b= new boolean[]{false, false, false, false, false, false};
+            b = new boolean[]{false, false, false, false, false, false};
         }
-       return b;
+        return b;
     }
-    public static void Hh_hh (Instruction instruction){
+
+    public static void Hh_hh(Instruction instruction) {
         boolean[] b = Haha(instruction);
-        String s = instruction.toString() +" = ";
+        String s = instruction.toString() + " = ";
         if (b[5])
-            s+= "opCode[5] ";
+            s += "opCode[5] ";
         else
-            s+="!opCode[5] ";
-        for(int i=1;i<b.length;i++)
-        {
-            if(b[5-i])
-                s+= "&& opCode["+(5-i)+"] ";
+            s += "!opCode[5] ";
+        for (int i = 1; i < b.length; i++) {
+            if (b[5 - i])
+                s += "&& opCode[" + (5 - i) + "] ";
             else
-                s+= "&& !opCode["+(5-i)+"] ";
+                s += "&& !opCode[" + (5 - i) + "] ";
         }
-        s=s.trim();
-        s+=";";
+        s = s.trim();
+        s += ";";
         System.out.println(s);
+    }
+
+
+    public static boolean[] fromStringtoBoolean(String s) {
+        boolean[] b = new boolean[s.length()];
+        for (int k = 0; k < s.length(); k++) {
+            if (s.charAt(k) == '1') {
+                b[s.length() - k - 1] = true;
+            } else {
+                b[s.length() - k - 1] = false;
+            }
+        }
+        return  b;
+    }
+
+
+    public static String fromBooleantoString (boolean[] b){
+        String s = "";
+        for(int i=0;i<b.length;i++){
+            if (b[b.length-i-1])
+                s+='1';
+            else
+                s+='0';
+        }
+        return s;
     }
 }
